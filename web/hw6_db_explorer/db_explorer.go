1d03ce180dbc0d0bc56a6903518eeb99ca9efacc
Igor Frolov <adromaryn@gmail.com>
2023-02-13 04:08:51 +0400

    refact list of tables: prefetch tables data on before server upload

--- web/hw6_db_explorer/db_explorer.go
+++ web/hw6_db_explorer/db_explorer.go
@@ -3,12 +3,20 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type TableField struct {
+	Field string
+	Type  string
+	Null  bool
+}
+
 type TablesListHandler struct {
-	DB *sql.DB
+	DB     *sql.DB
+	Tables map[string][]TableField
 }
 
 func (h *TablesListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -17,21 +25,13 @@ func (h *TablesListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rows, err := h.DB.Query("SHOW tables")
-	if err != nil {
-		log.Fatalf("Error querying tables: %v", err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
 	var tables []string
-	var table string
-	for rows.Next() {
-		rows.Scan(&table)
+	for table := range h.Tables {
 		tables = append(tables, table)
 	}
 
 	response := map[string]interface{}{"response": map[string]interface{}{"tables": tables}}
-	err = json.NewEncoder(w).Encode(response)
+	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
 		log.Fatalf("Error querying tables: %v", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -39,8 +39,43 @@ func (h *TablesListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewDbExplorer(db *sql.DB) (http.Handler, error) {
+
+	rows, err := db.Query("SHOW tables")
+	if err != nil {
+		log.Fatalf("Error querying tables: %v", err.Error())
+		panic(err)
+	}
+
+	tables := make(map[string][]TableField)
+	var table string
+	for rows.Next() {
+		rows.Scan(&table)
+
+		tables[table] = nil
+	}
+	rows.Close()
+
+	for table := range tables {
+		rows, err = db.Query(fmt.Sprintf("SHOW FULL COLUMNS FROM `%v`", table))
+		if err != nil {
+			log.Fatalf("Error querying table %v columns: %v", table, err.Error())
+			panic(err)
+		}
+
+		var fields []TableField
+		var field TableField
+		for rows.Next() {
+			rows.Scan(&field)
+			fields = append(fields, field)
+		}
+		rows.Close()
+
+		tables[table] = fields
+	}
+	rows.Close()
+
 	siteMux := http.NewServeMux()
-	handler := &TablesListHandler{db}
+	handler := &TablesListHandler{db, tables}
 	siteMux.Handle("/", handler)
 
 	return siteMux, nil
